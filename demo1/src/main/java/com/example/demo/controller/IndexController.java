e941b6ed401134a1e22f84ca27f514a6e56fbd86
yewuqin <45619448+yewuqin@users.noreply.github.com>
2018-12-06 13:59:20 +0800

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/one")
+    public String one(Model model) {
+        System.out.println("=========执行==========");
+        // 往model中放数据
+        model.addAttribute("score",18);
+
+        return "one";
+    }
+}
---
+++ demo1/src/main/resources/templates/one.ftl
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>index</title>
+</head>
+<body>
+<#if score gt 59>
+	及格！
+<#else>
+	不及格！
+</#if>
+
+<input type="button" value="进程序" onclick="location.href='index.htm'" />
+</body>
+</html>
