7b1462efb038d17267b3079d4941ba8ee9375d85
yewuqin <2212114607@qq.com>
2018-12-07 09:51:41 +0800

    第一次玩蛇

--- demo1/src/main/java/com/example/demo/controller/IndexController.java
+++ demo1/src/main/java/com/example/demo/controller/IndexController.java
@@ -16,7 +16,7 @@ public class IndexController {
     }
     @RequestMapping("/two.html")
     public String two(Model model) {
-    	model.addAttribute("score",45);
+    	model.addAttribute("score",55);
     	return "one";
     }
 }
